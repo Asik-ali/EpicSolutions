7933fe2fb7be84e32dede761b46e38385d6e9fed
Asik-ali <asik14923@gmail.com>
2024-01-19 19:43:21 +0530

    A

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,11 +5,19 @@ import Welcome from '../Components/Welcome/Welcome'
 import Footer from '../Components/Footer/Footer'
 import Newsletter from '../Components/newsletter/NewsLetter'
 import Contact from '../Components/contact/Contact'
+import AdSenseComponent from '../Components/Adsense/Adsense'
+import AdSenseComponent1 from '../Components/Adsense/Adsense1'
+import AdSenseComponent2 from '../Components/Adsense/Adsense2'
+import AdSenseComponent3 from '../Components/Adsense/Adsense3'
 function Home() {
   return (
     <>
-    <Hero/>
-    <Welcome/>
+      <AdSenseComponent />
+      <AdSenseComponent3 />
+      <Hero />
+      <Welcome />
+      <AdSenseComponent1 />
+      <AdSenseComponent2 />
     </>
   )
 }
