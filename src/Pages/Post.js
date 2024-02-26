04827973fdf8a38a511c9b55a19228d621869bd7
Asik-ali <asik14923@gmail.com>
2024-02-26 22:50:58 +0530

    A

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -11,11 +11,11 @@ const Post = ({ posts }) => {
     script.async = true;
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
-  
+
     // Push ads when the script is loaded
     script.onload = () => {
       const adsInsElements = document.querySelectorAll('.adsbygoogle');
-  
+
       // Check if 'ins' elements don't have child nodes (ads) before pushing
       adsInsElements.forEach((element) => {
         if (element.childNodes.length === 0) {
@@ -28,105 +28,119 @@ const Post = ({ posts }) => {
   const post = posts.find((post) => post.id.toString() === id);
 
   return (
-    <div className="relative max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-[#F8F9FD] mb-5">
+    <div className="flex gap-[20px]"> <div className="relative max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-[#F8F9FD] mb-5">
       <img src={post.img} alt={post.title} className="mb-4 rounded-lg w-full h-[400px] shadow-md" />
       <h1 className="text-3xl font-bold mb-4 text-center">{post.title}</h1>
       <section className="Absolute top-[350px] right-0">
-          <p>Absol</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="4430837000"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Absol</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="4430837000"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <p className="text-gray-700 mb-4">{post.outline}</p>
       <section className="container mx-auto mt-3">
-          <p>Ad</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="2647559035"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="2647559035"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <h4 className="text-2xl font-bold mb-4">{post.h1}</h4>
       <p className="text-gray-700 mb-4">{post.para1}</p>
       <section className="container mx-auto mt-3">
-          <p>Ad</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="8836157899"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="8836157899"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <h4 className="text-2xl font-bold mb-4">{post.h2}</h4>
       <p className="text-gray-700 mb-4">{post.para2}</p>
       <section className="container mx-auto mt-3">
-          <p>Ad</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="2793493727"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="2793493727"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <h4 className="text-2xl font-bold mb-4">{post.h3}</h4>
       <p className="text-gray-700 mb-4">{post.para3}</p>
-     
-        
+
+
       <h4 className="text-2xl font-bold mb-4">{post.h4}</h4>
       <p className="text-gray-700 mb-4">{post.para4}</p>
       <section className="container mx-auto mt-3">
-          <p>Ad</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="6018422869"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="6018422869"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <h4 className="text-2xl font-bold mb-4">{post.bodyh}</h4>
       <p>Ada</p>
       <ins
-            className="adsbygoogle"
-            style={{ display: 'block' , textAlign :"center" }}
-            data-ad-layout="in-article"
-            data-ad-format="fluid"
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="7248572032"
-          ></ins>
+        className="adsbygoogle"
+        style={{ display: 'block', textAlign: "center" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-2334117942638644"
+        data-ad-slot="7248572032"
+      ></ins>
       <h3 className="text-gray-700 mb-4">{post.body}</h3>
       <section className="container mx-auto mt-3">
-          <p>Ad</p>
-          {/* AdSense Ad Unit */}
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-2334117942638644"
-            data-ad-slot="6469014852"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-        </section>
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="6469014852"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+      </section>
       <h4 className="text-2xl font-bold mb-4">Conclusion</h4>
       <p className="text-gray-700 mb-4">{post.conclusion}</p>
+    </div>
+    <div>
+    <section className="container mx-auto mt-3">
+        <p>Ad</p>
+        {/* AdSense Ad Unit */}
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-format="autorelaxed"
+          data-ad-client="ca-pub-2334117942638644"
+          data-ad-slot="3547826719"
+        ></ins>
+      </section>
+    </div>
     </div>
   );
 };
