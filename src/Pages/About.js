17ca8b5fb5e96e192235d2d0c8167aa1716a8550
Asik-ali <asik14923@gmail.com>
2024-03-02 16:00:53 +0530

    A

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import AboutContent from '../Components/About/About'
+import React, { useEffect } from 'react';
+import AboutContent from '../Components/About/About';
 
 function About() {
+  useEffect(() => {
+    // This is equivalent to $(document).ready() in jQuery
+    // It ensures that the code inside will run after the DOM is fully loaded
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagservices.com/tag/js/gpt.js';
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      // This function is called after the Google Publisher Tag (GPT) script is loaded
+      window.googletag = window.googletag || { cmd: [] };
+      window.googletag.cmd.push(() => {
+        window.googletag.display('div-gpt-ad-1709375181924-0');
+      });
+    };
+
+    // Clean up function to remove the script when the component is unmounted
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
-        <AboutContent/>
+      <AboutContent />
+      <div id='div-gpt-ad-1709375181924-0' style={{ minWidth: '250px', minHeight: '50px' }} />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
